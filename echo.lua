2077d56fb1cd99411e84be2f680033d93fc88ef0
Firewolf <Firew0lf@users.noreply.github.com>
2015-06-06 13:26:53 +0200

    Create echo.lua

---
+++ echo.lua
@@ -0,0 +1,14 @@
+local args = {...}
+
+local str = ""
+if not args[1] then
+  str = io.stdin:read()
+else
+  for i=1, #args do
+    str = (str..tostring(args[i])
+  end
+end
+
+io.stdout:write(str)
+
+return 1, str
